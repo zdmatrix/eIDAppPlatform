cdf76c87c30c35b86adbff9def1434e8802f000c
zdmatrix <zdmatrix@163.com>
2014-11-05 17:09:02 +0800

    changed the title and icon, changed the default form file name to "MainForm.h"

--- eIDAppPlatform/GUI/MainForm.h
+++ eIDAppPlatform/GUI/MainForm.h
@@ -55,11 +55,19 @@ namespace GUI {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
+			this->SuspendLayout();
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(695, 470);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^  >(resources->GetObject(L"$this.Icon")));
+			this->Name = L"Form1";
+			this->Text = L"eID应用平台";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
